648f1483889ec08fb20d9cc37a60349a2f77cd1b
Nicholas Thiago Xavier Costa <nicholasxavis23@gmail.com>
2023-12-14 09:58:11 -0700

    merge : Musie branch to Main

---
+++ src/app/appointmentList/page.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function appointmentList() {
+    return (
+        <div>
+            <div>
+     <h2 className="text-bold text-2xl">Appointment</h2>
+     <div>hhhhhhhhhhh</div>
+     </div>
+        </div>
+    )
+}
--- src/app/appointments/page.jsx
+++ src/app/appointments/page.jsx
@@ -1,12 +1,25 @@
 "use client"
 import 'app/globals.css'
+import Link from 'next/link';
 
 // Page : Home
 const Appointments = () => {
 
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1> View Appoinment </h1>
+		<div className="flex  flex-col items-center justify-between p-24">
+			<h1 className='font-bold text-2xl  pb-8'> Appointments </h1>
+
+			<div className="flex">
+  <div className='flex mb-4 pt-4 pr-8'>
+    <Link className="bg-hcblue rounded-lg font-bold text-white py-2 px-9" href={"/addAppointment"}>Book Appointment</Link>
+  </div>
+
+  <div className='flex mb-4 pt-4 pr-8'>
+    <Link className="bg-hcblue rounded-lg font-bold text-white py-2 px-6" href={"/appointmentList"}>Previous Appointments</Link>
+  </div>
+</div>
+
+
 		</div>
 	)
 };
